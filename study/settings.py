8f2475eb6e871fe034fe829eed936cc40839a08e
tolujohn <tolujohnofficial@gmail.com>
2023-01-01 23:32:13 +0100

    updated

--- study/settings.py
+++ study/settings.py
@@ -76,15 +76,22 @@ WSGI_APPLICATION = 'study.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': "django.db.backends.postgresql_psycopg2",
+#         'HOST': "db.gpdsrhtpfushrscjakbz.supabase.co",
+#         'NAME': "postgres",
+#         'USER': "postgres",
+#         'PASSWORD': "@OLagBile2@",
+#         'PORT': "5432",
+
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'HOST': "db.gpdsrhtpfushrscjakbz.supabase.co",
-        'NAME': "postgres",
-        'USER': "postgres",
-        'PASSWORD': "@OLagBile2@",
-        'PORT': "5432",
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
