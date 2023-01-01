ce94720e3fcd663a98a914964434629d4d72c95c
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2023-01-01 21:27:29 +0100

    Update settings.py

--- maplehacks/settings.py
+++ maplehacks/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # <HINT> add your cloud host here
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
